eb984bf572bd6b0cf84f2fa06910bdd2f18ba1ba
chido <chidow@j5int.com>
2016-08-20 15:21:11 +0200

    editing the mustache unploader

--- Core/Elements/Uploader/Uploader.php
+++ Core/Elements/Uploader/Uploader.php
@@ -9,27 +9,36 @@
 class Uploader extends BaseElement
 {
 
-    function __construct($id, $label, ElementPermission $permissions, $elementPath='')
+    function __construct($id, $label, ElementPermission $permissions, $elementPath='', $elementCssClasses=[])
     {
         wp_enqueue_media();
         wp_register_script("wpAPIMediaUploader", __DIR__ . "wpAPIMediaUploader.js",  ["jquery"], "1.0.0", true);
         wp_enqueue_script("wpAPIMediaUploader");
 
-        parent::__construct($id, $label, $permissions, $elementPath);
+        if (empty($elementCssClasses))
+        {
+            $elementCssClasses = [
+                "button",
+                "button-primary"
+            ];
+        }
+
+        parent::__construct($id, $label, $permissions, $elementPath, $elementCssClasses);
     }
 
     function ReadView($post_id)
     {
-        echo $this->mustache->render('Text/read_view.mustache', ["value" => $this->GetDatabaseValue($post_id)]);
+        echo $this->mustache->render('Uploader/read_view.mustache', ["value" => $this->GetDatabaseValue($post_id)]);
     }
 
     function EditView( $post)
     {
         parent::EditView($post);
-        echo $this->mustache->render('Text/edit_view.mustache', [
+        echo $this->mustache->render('Uploader/edit_view.mustache', [
             "id" => $this->id,
             "label" => $this->label,
-            "value" => $this->GetDatabaseValue($post->ID)
+            "value" => $this->GetDatabaseValue($post->ID),
+            "cssClass" => implode(" ", $this->cssClasses)
         ]);
     }
 

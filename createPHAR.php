0fd975c52852fa5bdb34f542759d5c44b09da9af
chido <chidow@j5int.com>
2016-09-30 17:21:59 +0200

    Improving the phar generation method. Skipping uncessary folders

--- createPHAR.php
+++ createPHAR.php
@@ -7,6 +7,46 @@
  */
 ini_set('phar.readonly',"off");
 
+// Based on http://stackoverflow.com/a/20501275
+class MyRecursiveFilterIterator extends RecursiveFilterIterator {
+
+    public static $FILTERS = array(
+        '.svn',
+    );
+
+    public function accept() {
+        return !in_array(
+            $this->current()->getFilename(),
+            self::$FILTERS,
+            true
+        );
+    }
+
+}
+
+$exclude = array('.git', 'Docs', 'Build');
+
+$filter = function ($file, $key, $iterator) use ($exclude) {
+    if ($iterator->hasChildren() && !in_array($file->getFilename(), $exclude)) {
+        return true;
+    }
+    return $file->isFile();
+};
+
+$innerIterator = new RecursiveDirectoryIterator(
+    getcwd(),
+    RecursiveDirectoryIterator::SKIP_DOTS
+);
+$iterator = new RecursiveIteratorIterator(
+    new RecursiveCallbackFilterIterator($innerIterator, $filter)
+);
+
+
+unlink('Build/wpAPI.phar');
+
 $phar = new Phar('Build/wpAPI.phar', 0, 'wpAPI.phar');
-$phar->buildFromDirectory( getcwd());
-$phar->setStub($phar->createDefaultStub('wpAPI.php','wpAPI.php' ));
+$phar->buildFromIterator(
+    new RecursiveIteratorIterator(
+        new RecursiveCallbackFilterIterator($innerIterator, $filter)),
+    getcwd());
+$phar->setStub($phar->createDefaultStub('cli/index.php', 'www/index.php'));

3c3ad839f76b48b605dee70d5ea6d05e956cf1dc
chido <chidow@j5int.com>
2016-10-29 14:15:20 +0200

    Allowing to have more than one uploader on one page. Previous the one declared last would overwrite the rest.

--- Core/Elements/Uploader/wpAPIMediaUploader.js
+++ Core/Elements/Uploader/wpAPIMediaUploader.js
@@ -1,26 +1,40 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(function($) {
 
-    var mediaUploader;
+    var mediaUploaders = {};
 
-    $("#"+uploaderJsData.id+uploaderJsData.upload_button).on("click", function(e) {
-        e.preventDefault();
+    i = 1;
 
-        if (mediaUploader) {
-            mediaUploader.open();
-            return;
-        }
 
-        mediaUploader = wp.media({
-                title: uploaderJsData.title,
-                button: {
-                    text: uploaderJsData.buttonText
-                },
-                multiple: uploaderJsData.multiple == "true"
-            });
+    while (window.hasOwnProperty("uploaderJsData_bag_"+i)) {
 
-        mediaUploader.on("select", function(){
-            uploadedItem = mediaUploader.state().get('selection').first().toJSON();
+        uploaderJsData = window["uploaderJsData_bag_"+i];
 
+        $("#" + uploaderJsData.id + uploaderJsData.upload_button).on("click", {p_uploaderJsData: uploaderJsData},  function (e) {
+            //Tricky js. Needed to do this (pass the object) as it was using the global one, which will be set as the last element
+            uploaderJsData = e.data.p_uploaderJsData;
+            e.preventDefault();
+
+            if (mediaUploaders[uploaderJsData.id]) {
+                mediaUploaders[uploaderJsData.id].open();
+                return;
+            }
+            loadMediaUploader(uploaderJsData);
+        });
+        i++;
+    }
+    loadMediaUploader = function (uploaderJsData) {
+
+
+        mediaUploaders[uploaderJsData.id] = wp.media({
+            title: uploaderJsData.title,
+            button: {
+                text: uploaderJsData.buttonText
+            },
+            multiple: uploaderJsData.multiple == "true"
+        });
+
+        mediaUploaders[uploaderJsData.id].on("select", function () {
+            uploadedItem = mediaUploaders[uploaderJsData.id].state().get('selection').first().toJSON();
 
 
             var uploadedDataItem = JSON.stringify({
@@ -29,15 +43,13 @@ jQuery(document).ready(function($){
                 filename: uploadedItem.filename
             });
             //TODO:Change the id extensions
-            $("#"+uploaderJsData.id+uploaderJsData.selected_file).val(uploadedDataItem);
-            $("#"+uploaderJsData.id+uploaderJsData.selected_file_preview+" img").attr("src",uploadedItem.url);
+            $("#" + uploaderJsData.id + uploaderJsData.selected_file).val(uploadedDataItem);
+            $("#" + uploaderJsData.id + uploaderJsData.selected_file_preview + " img").attr("src", uploadedItem.url);
 
-            $("#"+uploaderJsData.id+uploaderJsData.selected_file_display).html(uploadedItem.filename);
+            $("#" + uploaderJsData.id + uploaderJsData.selected_file_display).html(uploadedItem.filename);
         });
 
-        mediaUploader.open();
+        mediaUploaders[uploaderJsData.id].open();
 
-        });
-
-
+    }
 });

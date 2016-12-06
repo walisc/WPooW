333f4cb590664b523d6f717dd3be275b9e3333d3
chido <chidow@j5int.com>
2016-12-07 01:04:34 +0200

    Adding Rich Text Control

--- Core/Elements/RichTextArea/RichTextArea.php
+++ Core/Elements/RichTextArea/RichTextArea.php
@@ -14,39 +14,27 @@ class RichTextArea extends BaseElement
 
     function __construct($id, $label, $permissions=null, $elementPath='')
     {
-        //wp_register_script("TinymceJs", DIRECTORY_SEPARATOR.  $this->GetElementDirectory()  . "tinymce/tinymce.min.js",  ["jquery"], "1.0.0");
-       // wp_register_script("wpAPIRichTextJs", DIRECTORY_SEPARATOR.  $this->GetElementDirectory()  . "wpAPIRichText.js",  ["jquery"], "1.0.0", true);
-
         parent::__construct($id, $label, $permissions, $elementPath);
     }
 
     function ReadView($post_id)
     {
-        echo $this->twigTemplate->render(get_class($this).'/read_view.mustache', ["value" => $this->GetDatabaseValue($post_id)]);
+        echo $this->twigTemplate->render(get_class($this).'/read_view.mustache', ["value" => html_entity_decode($this->GetDatabaseValue($post_id))]);
     }
 
     function EditView( $post)
     {
        parent::EditView($post);
 
-      // wp_enqueue_script("TinymceJs");
-
-      // wp_localize_script("wpAPIRichTextJs", "wpAPIRichTextJsData", ["id" =>$this->id]);
-     //  wp_enqueue_script("wpAPIRichTextJs");
-
-        echo $this->twigTemplate->render(get_class($this).'/edit_view.mustache', [
-           "id" => $this->id,
-           "label" => $this->label,
-           "value" => $this->GetDatabaseValue($post->ID)
-       ]);
-
-      //  wp_editor( $this->GetDatabaseValue($post->ID), $this->id );
+        wp_editor(  html_entity_decode($this->GetDatabaseValue($post->ID)) , "tinymce_".$this->id, array(
+            'tinymce' => true
+        ) );
     }
 
     function ProcessPostData($post_id)
     {
         parent::ProcessPostData($post_id);
-        $data = sanitize_text_field($_POST[$this->id]);
+        $data = esc_html($_POST["tinymce_".$this->id]);
 
         $this->SaveElementData($post_id, $data);
         

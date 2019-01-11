25490d7e24ec12ed6019beab815c501504a6af63
Chido <warambwac@gmail.com>
2019-01-11 14:50:57 +0200

    Adding unittests infrastructure

---
+++ tests/Core/PageTypes/SettingPageTest.php
@@ -0,0 +1,46 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class SettingPageTest extends TestCase{
+
+    function testCanCreateSettingPage(): void{
+
+    }
+
+    function testCanAddSectionPage(){
+
+    }
+
+    function testCanAddFieldsToSectionDirectly(){
+
+    }
+
+    function testCanAddFieldsToSectionIndividually(){
+
+    }
+
+    function testCanAddFieldsWithOutSection(){
+
+    }
+
+    function testCanSaveSettings(){
+
+    }
+
+    function testBeforeSaveFunction(){
+
+    }
+
+    function testUpdateSettingPageStyle(){
+
+    }
+
+    function testCustomSettingTemplate(){
+        
+    }
+
+
+
+}

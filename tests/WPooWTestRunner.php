d92abea75b521b000d963851fed8a6f3e5211593
Chido <warambwac@gmail.com>
2019-01-14 11:45:04 +0200

    Updates to the wpoowtestrunner

--- tests/WPooWTestRunner.php
+++ tests/WPooWTestRunner.php
@@ -2,15 +2,10 @@
 
 echo "--- Staring WPooW Tests --- ";
 
-$ch=curl_init('http://localhost/wpoow_2_0/wp-admin/admin-ajax.php');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER , true);
-curl_setopt($ch, CURLOPT_POSTFIELDS, "action=wpoow_testing_request");
+// 1. Linking files if need be
 
-$result = curl_exec($ch);
-
+$result = ProcessRequest('http://localhost/wpoow_2_0/wp-admin/admin-ajax.php');
 $resultDic = json_decode(substr($result,0,strlen($result)-1), true);
-curl_close($ch);
 
 if (!$resultDic["WPooWLinked"]){
     Logger::INFO("WPooW project not linked to WPooW test Plugin. Linking now");
@@ -23,9 +18,61 @@ if (!$resultDic["WPooWLinked"]){
     exec("composer install -d ". $resultDic["pluginPathDir"]);
     Logger::INFO("WPooW project linked successfully");
 
+    
 }
 
+// 2. installing Selenium dependencies
+if (!file_exists("./bin"))
+{
+    mkdir("./bin");
+}
 
+if (!file_exists("./bin/seleniumServer.jar")){
+    Logger::INFO("Selenium Server is not installed. Installing now");
+    ProcessFileRequest("http://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar",
+                        fopen(sprintf("%s%s%s%s%s",__DIR__, DIRECTORY_SEPARATOR, "bin", DIRECTORY_SEPARATOR, "seleniumServer.jar"), "w+"));
+}
+
+if (!file_exists("./bin/chromeDrivers.zip")){
+    Logger::INFO("Installing Selenium Chrome drivers");
+
+    $dirveType = "";
+    if (stristr(PHP_OS, 'DAR')){
+
+    }else if (stristr(PHP_OS, 'WIN')){
+
+    }else if(stristr(PHP_OS, 'LINUX')){
+
+    }
+    ProcessFileRequest("http://selenium-release.storage.googleapis.com/3.8/selenium-server-standalone-3.8.1.jar",
+                        fopen(sprintf("%s%s%s%s%s",__DIR__, DIRECTORY_SEPARATOR, "bin", DIRECTORY_SEPARATOR, "seleniumServer.jar"), "w+"));
+}
+
+wget https://chromedriver.storage.googleapis.com/2.34/chromedriver_linux64.zip
+unzip https://chromedriver.storage.googleapis.com/2.34/chromedriver_linux64.zip
+sudo mv -i chromedriver /usr/bin/.
+
+
+// Helper functions
+
+function ProcessFileRequest($url, $filePath){
+    $filePath = fopen(sprintf("%s%s%s",__DIR__, DIRECTORY_SEPARATOR, "seleniumServer.jar"), "w+");
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_FILE, $filePath);
+    curl_exec($ch);
+    curl_close($ch);
+}
+
+function ProcessRequest($url){
+    $ch=curl_init('http://localhost/wpoow_2_0/wp-admin/admin-ajax.php');
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER , true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "action=wpoow_testing_request");
+    
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
 
 Class Logger{
 

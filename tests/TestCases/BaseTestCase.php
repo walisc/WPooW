af841e1be39af270dfd5de48924bb6d7a18b3857
chidow <chidow@j5int.com>
2019-01-22 14:39:39 +0200

    Refactoring the BaseTest Class

--- tests/TestCases/BaseTestCase.php
+++ tests/TestCases/BaseTestCase.php
@@ -7,19 +7,23 @@ use \Facebook\WebDriver\Remote\RemoteWebDriver;
 use \Facebook\WebDriver\Remote\DesiredCapabilities;
 
 global $binPath;
+global $hosturl;
 $binPath = realpath(sprintf("%s%s%s%s%s%s",__DIR__, DIRECTORY_SEPARATOR,"..", DIRECTORY_SEPARATOR, "bin", DIRECTORY_SEPARATOR));
+$hosturl = "http://localhost:4444/wd/hub";
 
 abstract class BaseTestCase extends TestCase{
 
-    protected $hosturl = "http://localhost:4444/wd/hub";
+    function __construct(){
+        global $binPath, $hosturl;
+        putenv('webdriver.chrome.driver='.$binPath. DIRECTORY_SEPARATOR.'chromedriver');
+        putenv('PATH=' . getenv('PATH') . PATH_SEPARATOR . $binPath);
+        RemoteWebDriver::create($hosturl, DesiredCapabilities::chrome());
+    }
 
     protected function setUp()
     {
-        global $binPath;
-        putenv('webdriver.chrome.driver='.$binPath. DIRECTORY_SEPARATOR.'chromedriver');
-        putenv('PATH=' . getenv('PATH') . PATH_SEPARATOR . $binPath);
-        RemoteWebDriver::create($this->hosturl, DesiredCapabilities::chrome());
     }
+    
     abstract function LoadElements($WPooW);
 }
 

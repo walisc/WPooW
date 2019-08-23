f3c94a865bba0e118e95afd55a2ef17e4f1e003e
chidow <chidow@j5int.com>
2019-08-23 15:04:56 +0200

    Adding the PermissionHelper for testing

--- Tests/WPooWTestsInputer.php
+++ Tests/WPooWTestsInputer.php
@@ -70,6 +70,8 @@ class WPooWInputerBase{
     protected $driver;
     protected $isEditable = true;
 
+    use WPooWTestPermissionsHelper;
+
     function __construct($parent)
     {
        $this->parent = $parent;
@@ -85,54 +87,6 @@ class WPooWInputerBase{
         return $extraArgs;
     }
 
-    protected function checkFieldPermissions($gridClosureFunc, $addClosureFunc, $editClosureFunc, $field, $pageType){
-        $permissions = $this->getFieldPermissions($field, $pageType);
-
-
-        if (array_key_exists(WPooWTestsConsts::PERMISSIONS_ALL, $permissions)){
-            return true;
-        }
-
-        if ($pageType == WPooWTestsConsts::PAGE_TYPE_GRID)
-        {
-            $this->parent->assertTrue($gridClosureFunc($permissions));
-        }
-        else if ($pageType == WPooWTestsConsts::PAGE_TYPE_ADD){
-            $this->parent->assertTrue($addClosureFunc($permissions));
-        }
-        else if ($pageType == WPooWTestsConsts::PAGE_TYPE_EDIT){
-            $this->parent->assertTrue($editClosureFunc($permissions));
-        }
-
-
-        return $this->isEditable;
-    }
-
-
-    private function getFieldPermissions($field, $pageType){
-
-        $permissionArray = [];
-
-        if (!array_key_exists('permissions', $field)){
-            return [WPooWTestsConsts::PERMISSIONS_ALL];
-        }
-        if (!array_key_exists($pageType, $field['permissions'])){
-            return [WPooWTestsConsts::PERMISSIONS_ALL];
-        }
-        else{
-            $permissionString = $field['permissions'][$pageType];
-            if (strpos($permissionString, 'c') !== false){
-                array_push($permissionArray, WPooWTestsConsts::PERMISSIONS_CREATE);
-            }
-            if (strpos($permissionString, 'r') !== false){
-                array_push($permissionArray, WPooWTestsConsts::PERMISSIONS_READ);
-            }
-            if (strpos($permissionString, 'u') !== false){
-                array_push($permissionArray, WPooWTestsConsts::PERMISSIONS_UPDATE);
-            }
-        }
-        return $permissionArray;
-    }
 }
 
 class BaseTextInputer extends WPooWInputerBase{
@@ -162,53 +116,7 @@ class BaseTextInputer extends WPooWInputerBase{
 
     function checkPermission($postTypeID, $field, $pageType, $returnCanEdit=false)
     {
-        $gridPageClosureFunc = function($permissions) use ($field, $pageType) {
-            return true;
-
-        };
-
-        $addPageClosureFunc = function($permissions) use ($postTypeID, $field, $pageType) { //On Add Page
-            if (in_array(WPooWTestsConsts::PERMISSIONS_CREATE, $permissions) || in_array(WPooWTestsConsts::PERMISSIONS_UPDATE, $permissions)){
-                // find element but should be editable
-                $this->isEditable = true;
-                try {
-                    $this->driver->findElement($this->getSelector($postTypeID, $field));
-                    return true;
-                }catch (NoSuchElementException $e){
-                    return false;
-                }
-            }else if (in_array(WPooWTestsConsts::PERMISSIONS_READ, $permissions)){
-                //find element in read only
-                $this->isEditable = false;
-                try {
-                    $this->driver->findElement($this->getSelector($postTypeID, $field));
-                    return true;
-                }catch (NoSuchElementException $e){
-                    try {
-                        $this->driver->findElement(WebDriverBy::xpath("//div[@id='${postTypeID}_${field['id']}' and contains(@class,'postbox')]"));
-                        return true;
-                    }catch(NoSuchElementException $e){
-                        return false;
-                    }
-                }
-            }
-            else{
-                // should not exists, as not element specified
-                $this->isEditable = false;
-                try {
-                    $this->driver->findElement($this->getSelector($postTypeID, $field));
-                    return false;
-                }catch (NoSuchElementException $e){
-                    return true;
-                }
-            }
-        };
-
-        $editPageClosureFunc = function($permissions) use ($postTypeID, $field, $pageType) {
-            return true;
-        };
-
-        return $this->checkFieldPermissions($gridPageClosureFunc, $addPageClosureFunc, $editPageClosureFunc , $field, $pageType);
+        return $this->checkPermissionsText($postTypeID, $field, $pageType, $returnCanEdit);
     }
 }
 class TextInputer extends BaseTextInputer implements ElementInputer {
